ced9d879e5808a0bf2a4359598285572af559e0e
Mi Peter <125556028+MiPeeter@users.noreply.github.com>
2024-04-06 18:04:19 +0530

    sadf

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,20 @@
+
 import React from 'react'
 import { Hero } from './Hero/Hero'
 import { About } from './HomeAbout/About'
+import { Finance } from "./Finance/Finance";
+import RoadmapComponent from "./Roadmap/Roadmap";
+import { AboutSatoshi } from './SatoshiFX/AboutSatoshi';
 
- 
+
+
 export function Home() {
   return (
     <>
       <Hero />
       <About />
-
+      <AboutSatoshi />
+      <Finance />
       <RoadmapComponent />
     </>
   );
